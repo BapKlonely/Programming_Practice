3246546b39f3728a360c2528ed6e05c6268683d0
Dan Lan <3195221300@qq.com>
2026-07-08 15:36:33 +0800

    修改代码错误并完善注释信息

--- struct/user.h
+++ struct/user.h
@@ -1,5 +1,5 @@
-#ifndef USER_HPP
-#define USER_HPP
+#ifndef USER_H
+#define USER_H
 #include <QString>
 /*************************/
 //通过枚举初步筛选用户类型//
@@ -9,6 +9,10 @@ enum class user_type
     consumer=1,
     administrator=2
 };
+//游客（仅能查看车次信息）：0
+//乘客（拥有账号，可以查看车次信息，可以订票，退票等操作）：1
+//管理员（拥有账号，可以查看车次信息，可以订票，退票等操作，可以管理用户信息）：2
+/*************************/
 struct user_information
 {
     QString account;//用户名
@@ -18,4 +22,5 @@ struct user_information
     bool enabled_account;//账户是否启用
     user_type type;//用户类型
 };
+//给出用户的账号，密码，姓名，是否为管理员，账户是否启用，用户类型
 #endif
