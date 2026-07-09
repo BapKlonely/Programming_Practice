78af178424c808bd35273de6efd326da91cd6941
somebody-kang <2579311246@qq.com>
2026-07-09 16:12:32 +0800

    Delete build directory

--- build/CMakeFiles/train_ticket_system.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for train_ticket_system.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/train_ticket_system_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/train_ticket_system_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
